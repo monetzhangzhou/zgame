8893abbe072c5679df37dfd43e2545b674c5fdb9
monetzhangzhou <monetzhangzhou@gmail.com>
2018-04-02 21:04:48 +0800

    netty config

---
+++ src/main/java/zgame/net/netty/Server/handler/GameDecodeHandler.java
@@ -0,0 +1,13 @@
+package zgame.net.netty.Server.handler;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author zhangzhou
+ * @date 2018-04-02 消息解码
+ */
+public class GameDecodeHandler extends ChannelInboundHandlerAdapter {
+
+	private static final int MAX_LENGTH = 2048;
+
+}
---
+++ src/main/java/zgame/net/netty/Server/handler/GameEncodeHandler.java
@@ -0,0 +1,11 @@
+package zgame.net.netty.Server.handler;
+
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+
+/**
+ * @author zhangzhou
+ * @date 2018-04-02 消息编码
+ */
+public class GameEncodeHandler extends ChannelOutboundHandlerAdapter {
+
+}
